c614572bd57c2ffc903a4ca38fe57ef16e7d2e14
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-21 21:48:21 -0600

    adding hero section and links to individual blog posts

---
+++ Bloggie/Bloggie.Web/Pages/Blog/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bloggie.Web.Pages.Blog
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet(string urlHandle)
+        {
+        }
+    }
+}
