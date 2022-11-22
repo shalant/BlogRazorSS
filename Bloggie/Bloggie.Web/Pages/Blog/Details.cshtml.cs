7f314c723083984766e7e49e1d7865caa24a5ca4
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-21 22:22:13 -0600

    showing details

--- Bloggie/Bloggie.Web/Pages/Blog/Details.cshtml.cs
+++ Bloggie/Bloggie.Web/Pages/Blog/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace Bloggie.Web.Pages.Blog
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet(string urlHandle)
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPost BlogPost { get; set; }
+
+        public DetailsModel(IBlogPostRepository blogPostRepository)
         {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        public async Task<IActionResult> OnGet(string urlHandle)
+        {
+            BlogPost = await blogPostRepository.GetAsync(urlHandle);
+            return Page();
         }
     }
 }
