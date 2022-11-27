8e163de39cc1f900e5f809c0963590b112b40ab7
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-27 09:15:26 -0600

    working on displaying table of users

--- Bloggie/Bloggie.Web/Pages/Admin/Users/Index.cshtml.cs
+++ Bloggie/Bloggie.Web/Pages/Admin/Users/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace Bloggie.Web.Pages.Admin.Users
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserRepository userRepository;
+
+        public List<User> Users { get; set; }
+
+        public IndexModel(IUserRepository userRepository)
         {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var users = await userRepository.GetAll();
+
+            Users = new List<User>();
+            foreach(var user in users)
+            {
+                Users.Add(new Models.ViewModels.User()
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName,
+                    Email = user.Email
+                });
+            }
+
+            return Page();
         }
     }
 }
